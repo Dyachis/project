a39fdfe10d94d7f0f57978b7e1c3c7039503398a
Aaron Vasilev <oigen711@gmail.com>
2021-03-17 13:30:31 +0200

    Add styled buttons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
+import Button from './components/styled/Button'
+
+const theme = {
+  color: '#0F4C81',
+  background: '#ffff'
+}
 
 function App() {
   return (
-    <div className="App">Hello World</div>
+    <div className="App">Hello World
+      <Button primary>Sing up</Button>
+      <Button>Find place</Button>
+    </div>
   );
 }
 
